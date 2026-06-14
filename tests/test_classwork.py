f381d514012d342c458691c42f00f5fa691c1c74
MatveyPrybyshchuk <matvey.pr.v@gmail.com>
2026-06-14 22:16:47 +0300

    updated profile_el file

--- tests/test_classwork.py
+++ tests/test_classwork.py
@@ -1,33 +1,33 @@
+import json
+
 import pytest
-from playwright.sync_api import Page, BrowserContext, expect
+import requests
 
 
-# '''
-# Для 3 страниц описать PageObject, так чтобы в сумме было 6 тестов, на ваш проект
 
-# 1 Login
-# 2 Главная
-# 3 все доски 
-# 4 все задачи 
-# Автотесты 
-# 1 Отображение страниц
-# 2 Авторизация 
-# 3 Отображение информации о пользователе
-# 4 Выход из приложения
-# '''
+# @pytest.mark.api
+# def test_health_check():
+#     response = requests.get('http://localhost:8000/')
+#     assert response.status_code == 200
 
-# @pytest.mark.play
-# def test_check(page: Page, context: BrowserContext):
-#     page.goto("https://letcode.in/window")
+#     response_json = response.json()
 
-#     page.locator('#multi').click()
-    
-#     all_pages = context.pages
-
-#     new_pages = [p for p in all_pages if p != page]
+#     assert response_json['message'] == 'Task Management System API'
+#     assert response_json['version'] ==  '1.0.0'
+#     assert response_json['docs'] ==  '/docs'
+#     assert response_json['redoc'] ==  '/redoc'
+#     assert response_json['health'] ==  '/health'
 
 
-#     expect(new_pages[0]).to_have_url("https://letcode.in/alert")
-#     expect(new_pages[1]).to_have_url("https://letcode.in/dropdowns")
+@pytest.mark.regis
+def test_register():
+    body = json.dumps({
+        "username": "user2@example.com",
+        "email": "user2@example.com",
+        "password": "Qwerty123!",
+    })
 
-#     # new_page.wait_for_load_state("load", timeout=30000)
+    response = requests.post('http://localhost:8000/auth/register', data = body)
+    breakpoint()
+
+    assert response.status_code == 201
