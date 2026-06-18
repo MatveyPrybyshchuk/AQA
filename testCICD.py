c7f846fafbab2469b787b6ac570da66d99c8d1f0
MatveyPrybyshchuk <matvey.pr.v@gmail.com>
2026-06-18 23:40:04 +0300

    ff

---
+++ testCICD.py
@@ -0,0 +1,6 @@
+"""module docstring"""
+def checkCICD():
+    """function docstring"""
+    a = 2
+    b = a + 1
+    print(b)
--- test_CICD.py
+++
@@ -1,5 +0,0 @@
-
-def check_CI_CD():
-    a = 2
-    b = a + 1
-    print(b)
