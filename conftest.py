778d355dd9943be9c6a730af98d59e88a08013c3
MatveyPrybyshchuk <matvey.pr.v@gmail.com>
2026-06-18 23:25:41 +0300

    Test CI CD

--- conftest.py
+++
@@ -1,9 +0,0 @@
-
-import pytest
-
-from services.auth_service import AuthentificationService
-
-
-@pytest.fixture(scope = "session")
-def admin_token():
-    return AuthentificationService().login('admin@example.com', 'admin123').access_token
--- steps/registration_step.py
+++
@@ -1,7 +0,0 @@
-from services.auth_service import AuthentificationService
-
-
-def registration_user(user_name, email, password = "Qwerty123!"):
-    response = AuthentificationService().registration_user(user_name, email, password)
-
-    return response.access_token
