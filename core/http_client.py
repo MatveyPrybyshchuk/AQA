2b5fd80e6510be347da59a5dc0b5b401d1e1a98e
MatveyPrybyshchuk <matvey.pr.v@gmail.com>
2026-07-13 22:30:18 +0300

    pylint fix

--- core/http_client.py
+++ core/http_client.py
@@ -6,35 +6,46 @@ from core.log_base import logger
 
 class HttpClient:
     DOMAIN = os.getenv('BASE_URL', 'http://localhost:8000')
-    
-    def make_request(self, method, url, body: dict | None = None, headers: dict | None = None):
-        response = requests.request(method, self.DOMAIN + url, data=json.dumps(body) if body else None, headers=headers)
+    DEFAULT_TIMEOUT = 30
+
+    def make_request(self, method, url, body: dict | None = None, headers: dict | None = None, timeout: int | None = None):
+
+        if timeout is None:
+            timeout = self.DEFAULT_TIMEOUT
+
+        response = requests.request(
+            method,
+            self.DOMAIN + url,
+            data=json.dumps(body) if body else None,
+            headers=headers,
+            timeout=timeout
+        )
 
         try:
             response.raise_for_status()
 
             if not response.text:
-                logger.info(f'{response.url}: No content (status {response.status_code})')
+                logger.info('%s: No content (status %s)', response.url, response.status_code)
                 return None
-        
+
             response_json = response.json()
-            logger.info(f'{response.url}:: {response_json}')
+            logger.info('%s:: %s', response.url, response_json)
 
             return response_json
-        
+
         except requests.exceptions.HTTPError as err:
-            logger.error(f'HTTP Error: {err}')
+            logger.error('HTTP Error: %s', err)
 
             raise
 
     def get(self, url, headers: dict | None = None):
-        return self.make_request('GET', url, headers=headers)   
+        return self.make_request('GET', url, headers=headers)
 
     def post(self, url, body: dict | None = None, headers: dict | None = None):
         return self.make_request('POST', url, body=body, headers=headers)
-    
+
     def put(self, url, body: dict | None = None, headers: dict | None = None):
         return self.make_request('PUT', url, body=body, headers=headers)
-    
+
     def delete(self, url, headers: dict | None = None):
         return self.make_request('DELETE', url, headers=headers)
